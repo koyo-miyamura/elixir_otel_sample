75364f75794792d178183b60c6926cf9fc02c57a
koyo-miyamura <nexus9071@gmail.com>
2025-12-06 14:52:10 +0900

    docker compose exec web mix phx.gen.json Accounts User users name:string

--- lib/otel_sample_web/router.ex
+++ lib/otel_sample_web/router.ex
@@ -7,6 +7,8 @@ defmodule OtelSampleWeb.Router do
 
   scope "/api", OtelSampleWeb do
     pipe_through :api
+
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   # Enable LiveDashboard and Swoosh mailbox preview in development
